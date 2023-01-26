c519e31cb44c4a75ad2331f2ee0aeda86f939f5f
ADARSH KUMAR LAL <80323632+8-adarsh@users.noreply.github.com>
2023-01-26 22:03:40 +0530

    Add files via upload

---
+++ alpha.java
@@ -0,0 +1,19 @@
+public class alpha {
+
+    public static void main(String[] args) {
+        char ch='A'; //65
+        while(ch<=90){
+            System.out.print(ch+" ");
+            ch++ ; 
+        }
+
+        //Do while
+        
+        char ch1 ='A';
+        do{
+           System.out.print(ch+" ");
+           ch++ ;
+        }while(ch<='Z');
+    }
+    
+}
